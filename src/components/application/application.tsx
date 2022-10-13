e298673fae6831a9d70bd4f2a4b2d5140abb0a24
Peter Yoo <trustintofu@gmail.com>
2022-10-12 18:09:08 -0700

    wip: adds value attribute to input element for testing purposes.

--- src/components/application/application.tsx
+++ src/components/application/application.tsx
@@ -7,7 +7,13 @@ export const Application = () => {
       <form>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="Fullname" />
+          <input
+            type="text"
+            id="name"
+            placeholder="Fullname"
+            value="Peter"
+            onChange={() => {}}
+          />
         </div>
         {/* multiple text roles, use the text inside label tag instead */}
         <div>
